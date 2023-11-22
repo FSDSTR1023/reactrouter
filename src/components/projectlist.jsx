dbcf93efe0b7cc05b9cdefa35cf1c23df87b7c45
Jordi Galobart Salvat <jgalobart@gmail.com>
2023-11-22 17:32:58 +0100

    add project list

--- src/components/projectlist.jsx
+++ src/components/projectlist.jsx
@@ -2,6 +2,11 @@ function ProjectList() {
     return (
         <>
             <h1>Project List</h1>
+            <ul>
+                <li>Project 1</li>
+                <li>Project 2</li>
+                <li>Project 3</li>
+            </ul>
         </>
     )
 }
